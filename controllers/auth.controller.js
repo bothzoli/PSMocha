b92a148672ef3ba00d87613ab7b3376bfa1e9e76
Zoltan Both <zoltan.both@sap.com>
2019-07-01 07:38:29 +0200

    Testing with Mocha

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,9 +1,26 @@
 function AuthController() {
-  function isAuthorized(roles, neededRole) {
+  let roles;
+
+  function setRoles(role) {
+    roles = role;
+  }
+  function isAuthorized(neededRole) {
     return roles.indexOf(neededRole) >= 0;
   }
+
+  function isAuthorizedAsync(neededRole, cb) {
+    setTimeout(
+      () => {
+        cb(roles.indexOf(neededRole) >= 0);
+      },
+      0
+    );
+  }
+
   return {
-    isAuthorized
+    setRoles,
+    isAuthorized,
+    isAuthorizedAsync
   };
 }
 
--- test/controllers/auth.controller.spec.js
+++ test/controllers/auth.controller.spec.js
@@ -3,13 +3,45 @@ const assert = require('assert');
 const authController = require('./../../controllers/auth.controller');
 
 describe('AuthController', function () {
+  beforeEach(function setupUserRole() {
+    authController.setRoles(['user']);
+  });
+
   describe('isAuthorized', function () {
     it('Should return false if not authorized', function () {
-      assert.equal(false, authController.isAuthorized(['user'], 'admin'));
+      assert.equal(false, authController.isAuthorized('admin'));
     });
 
-    it('Should return true if authorized', function () {
-      assert.equal(true, authController.isAuthorized(['admin'], 'admin'));
+    it.skip('Should return true if authorized', function () {
+      authController.setRoles(['user', 'admin']);
+      assert.equal(true, authController.isAuthorized('admin'));
+    });
+
+    it('Should not allow get if not authorized');
+
+    it('Should allow get if authorized');
+  });
+
+  describe('isAuthorizedAsync', function () {
+    it.only('Should return false if not authorized', function (done) {
+      authController.isAuthorizedAsync(
+        'admin',
+        (isAuth) => {
+          assert.equal(false, isAuth);
+          done();
+        }
+      );
+    });
+
+    it('Should return true if authorized', function (done) {
+      authController.setRoles(['user', 'admin']);
+      authController.isAuthorizedAsync(
+        'admin',
+        (isAuth) => {
+          assert.equal(true, isAuth);
+          done();
+        }
+      );
     });
   });
 });
